8346cda6fd64bb54cd867e4a86ad97c447ff58d5
Mantvydas Baranauskas <mantvydo@gmail.com>
2017-03-26 17:35:56 +0100

    Update ad-authentication.ps1

--- oas-worker/ad-authentication.ps1
+++ oas-worker/ad-authentication.ps1
@@ -1,10 +1,7 @@
 ﻿$Global:PASSWORD = ConvertTo-SecureString "" -AsPlainText -Force
 $Global:CREDENTIALS = New-Object System.Management.Automation.PSCredential(“”, $Global:PASSWORD)
-#$Global:CREDENTIALS = Get-Credential
-# $Global:CREDENTIALS = $Global:CREDENTIALS
 
 function getCredentials() {
-    
     if ($Global:CREDENTIALS -ne $null) {
         return $Global:CREDENTIALS
     } else {
@@ -12,7 +9,7 @@ function getCredentials() {
     }
 }
 
-function authenticate(){
+function authenticate() {
     $Global:CREDENTIALS = Get-Credential
     return $Global:CREDENTIALS
 }
