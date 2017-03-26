c1ef819ccda19ce05c47209a39f527b76277777a
mantvydas <mantvydas@localhost.localdomain>
2017-03-26 17:21:49 +0100

    added ability to retrieve domain registrant date, organisation, country, state, city

--- whois.py
+++ whois.py
@@ -3,12 +3,14 @@ import urllib.request
 import json
 import sys
 
+BASE_WHOIS_URL = "https://www.whois.com/whois/"
+
 def sendGetRequest(url):
     response = urllib.request.urlopen(url).read().decode("utf-8")
     return response
 
 
-def fetchAllDNSRecords(domain):
+def fetchAllDNSRecordsByDomain(domain):
     recordsToLookUp = [
         "A",
         "MX",
@@ -18,14 +20,14 @@ def fetchAllDNSRecords(domain):
     ]
 
     for record in recordsToLookUp:
-        dnsRecords = lookupDNSRecordForHost(domain, record)
+        dnsRecords = getDNSRecordsByDomain(domain, record)
         printDNSRecords(dnsRecords)
 
 
-def lookupDNSRecordForHost(hostname, recordType):
+def getDNSRecordsByDomain(domain, recordType):
     baseUrl = "https://dns-api.org/"
     recordType += "/"
-    url = baseUrl + recordType + hostname
+    url = baseUrl + recordType + domain
     dnsRecordsRaw = sendGetRequest(url)
 
     return dnsRecordsRaw
@@ -40,20 +42,61 @@ def printDNSRecords(dnsRecordsRaw):
             print("Some DNS records may be missing..")
 
 
-def whoisDomain(domain):
-    url = "http://whoiz.herokuapp.com/lookup.json?url=" + domain
+def getWhoisRawByDomain(domain):
+    url = BASE_WHOIS_URL + domain
     whoisResponseRaw = sendGetRequest(url)
+
     return whoisResponseRaw
 
-def printWhois(whoisRaw):
-    whois = json.loads(whoisRaw)
-    print("Created: " + str(whois["created_on"]))
 
-if sys.argv.__len__() > 1:
-    domain = sys.argv[1]
-    fetchAllDNSRecords(domain)
-    whoisDomainRaw = whoisDomain(domain)
-    printWhois(whoisDomainRaw)
+def printRegistrantInfo(registrantInfo, domain):
+    print("Created:\t" + registrantInfo["created"] + " | " + registrantInfo["organisation"] + "\n"
+        "Location:\t" + registrantInfo["city"] + ", " + registrantInfo["state"] + ", " + registrantInfo["country"] + "\n"
+        "More:\t\t" + BASE_WHOIS_URL + domain
+          )
 
-else:
-    print("Supply a hostname like so whois.py [hostname]")
+
+def getRegistrantInfo(whoisRawData):
+    registrantAttributes = \
+        "Creation Date: ", \
+        "Registrant Organization: ", \
+        "Registrant City: ", \
+        "Registrant State/Province: ", \
+        "Registrant Country: "
+
+    registrantInfoKeys = ["created", "organisation", "city", "state", "country"]
+    registrantInfoValues = []
+
+    for index, attribute in enumerate(registrantAttributes):
+        registrantInfoValues.append(getRegistrantDataByAttribute(whoisRawData, attribute))
+
+    replaceEmptyItemsWithPlaceholders(registrantInfoValues)
+    registrantInfo = dict(zip(registrantInfoKeys, registrantInfoValues))
+
+    return registrantInfo
+
+
+def replaceEmptyItemsWithPlaceholders(registrantInfoValues):
+    for index, info in enumerate(registrantInfoValues):
+        if info is None:
+            registrantInfoValues[index] = "-"
+
+    return registrantInfoValues
+
+
+def getRegistrantDataByAttribute(whoisRawData, attribute):
+    if whoisRawData.find(attribute) > 0 :
+        return whoisRawData.split(attribute)[1].split("\n")[0]
+
+
+def main():
+    if sys.argv.__len__() > 1:
+        domain = sys.argv[1]
+        fetchAllDNSRecordsByDomain(domain)
+        whoisDomainRaw = getWhoisRawByDomain(domain)
+        registrantInfo = getRegistrantInfo(whoisDomainRaw)
+        printRegistrantInfo(registrantInfo, domain)
+    else:
+        print("Supply a domain like so: whois.py [domain]")
+
+main()
