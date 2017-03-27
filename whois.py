89b1ed7eb056e51fe24ea4919281a586a43d4b09
mantvydas <mantvydas@localhost.localdomain>
2017-03-27 23:26:06 +0100

    added simple error handling and housekeeping

--- whois.py
+++ whois.py
@@ -2,8 +2,7 @@
 import urllib.request
 import json
 import sys
-
-# todo: accept ip as an argument
+import socket
 
 BASE_WHOIS_URL = "https://www.whois.com/whois/"
 BASE_CYMON_URL = "https://cymon.io/api/nexus/v1/ip/"
@@ -11,7 +10,11 @@ BASE_CYMON_URL = "https://cymon.io/api/nexus/v1/ip/"
 def sendGetRequest(url):
     headers = {"Authorization": "Token 9dd9bc3276b0c35f5d64624bb7901f296b0ff37a"}
     request = urllib.request.Request(url, headers=headers)
-    response = urllib.request.urlopen(request).read().decode("utf-8")
+    try:
+        response = urllib.request.urlopen(request).read().decode("utf-8")
+    except:
+        print("Domain or IP you supplied may not be in correct format..")
+        exit(1)
     return response
 
 
@@ -26,15 +29,18 @@ def fetchAllDNSRecordsByDomain(domain):
 
     for record in recordsToLookUp:
         dnsRecords = getDNSRecordsByDomain(domain, record)
-        dnsRecordsDict = printDNSRecords(dnsRecords)
+        dnsRecordsDict = printDNSRecords(dnsRecords, record)
         getIPFromDNSRecord(dnsRecordsDict, record)
 
 
 def getIPFromDNSRecord(dnsRecords, record):
     global DOMAIN_IP
-    if record == "A" and  DOMAIN_IP == None:
-        DOMAIN_IP = dnsRecords[0]["value"]
-        print(DOMAIN_IP)
+    try:
+        if record == "A" and  DOMAIN_IP == None:
+            DOMAIN_IP = dnsRecords[0]["value"]
+    except:
+        print("Domain you entered could not be resolved, check spelling.")
+        exit(1)
 
 
 def getDNSRecordsByDomain(domain, recordType):
@@ -45,13 +51,14 @@ def getDNSRecordsByDomain(domain, recordType):
     return dnsRecordsRaw
 
 
-def printDNSRecords(dnsRecordsRaw):
+def printDNSRecords(dnsRecordsRaw, record):
     dnsRecords = convertStringToJSON(dnsRecordsRaw)
+
     for dnsRecord in dnsRecords:
         try:
             print(dnsRecord["type"] + "\t" + dnsRecord["value"] + "\t" + dnsRecord["name"])
         except:
-            print("Some DNS records may be missing..")
+            print(record + "\tDNS record seems missing..")
     return dnsRecords
 
 
@@ -113,7 +120,7 @@ def getThreatReportsByIP(ip):
 
 def printThreatReports(threatReports):
     if threatReports["count"] > 0:
-        print("\n\n[*] Printing threat reports for the domain, based on the IP: " + DOMAIN_IP)
+        print("\n\n[*] Some historical threat reports for " + DOMAIN_IP)
 
         for report in threatReports["results"]:
             print("[!] " + report["created"] + " " + report["title"] + " " + str(report["description"]).strip().replace(".","[.]"))
@@ -122,22 +129,36 @@ def printThreatReports(threatReports):
             if containsDetailsUrl:
                     print("More: " + report["details_url"] + "\n")
     else:
-        print("\n\n[*] No threat reports found for the domain, based on the IP: " + DOMAIN_IP)
+        print("\n\n[*] No threat reports found for " + DOMAIN_IP)
+
+
+def isValidIPAddress(string):
+    try:
+        socket.inet_aton(string)
+        return True
+    except:
+        return False
 
 
 def main():
     global DOMAIN_IP
     DOMAIN_IP = None
+
     if sys.argv.__len__() > 1:
         domain = sys.argv[1]
-        fetchAllDNSRecordsByDomain(domain)
-        whoisDomainRaw = getWhoisRawByDomain(domain)
-        registrantInfo = getRegistrantInfo(whoisDomainRaw)
-        printRegistrantInfo(registrantInfo, domain)
+
+        if not isValidIPAddress(domain):
+            fetchAllDNSRecordsByDomain(domain)
+            whoisDomainRaw = getWhoisRawByDomain(domain)
+            registrantInfo = getRegistrantInfo(whoisDomainRaw)
+            printRegistrantInfo(registrantInfo, domain)
+        else:
+            DOMAIN_IP = domain
         threatReports = getThreatReportsByIP(DOMAIN_IP)
         printThreatReports(threatReports)
+
     else:
-        print("Supply a domain like so: whois.py [domain]")
+        print("Supply a domain or IPv4 like so: whois.py [domain | IP]")
 
 
 main()
