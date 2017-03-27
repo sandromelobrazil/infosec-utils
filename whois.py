c318004969f42626f38f5a091b0c0139c999d02b
mantvydas <mantvydas@localhost.localdomain>
2017-03-27 20:36:24 +0100

    integrated threat reports for the domain

--- whois.py
+++ whois.py
@@ -4,9 +4,12 @@ import json
 import sys
 
 BASE_WHOIS_URL = "https://www.whois.com/whois/"
+BASE_CYMON_URL = "https://cymon.io/api/nexus/v1/ip/"
 
 def sendGetRequest(url):
-    response = urllib.request.urlopen(url).read().decode("utf-8")
+    headers = {"Authorization": "Token 9dd9bc3276b0c35f5d64624bb7901f296b0ff37a"}
+    request = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(request).read().decode("utf-8")
     return response
 
 
@@ -21,7 +24,15 @@ def fetchAllDNSRecordsByDomain(domain):
 
     for record in recordsToLookUp:
         dnsRecords = getDNSRecordsByDomain(domain, record)
-        printDNSRecords(dnsRecords)
+        dnsRecordsDict = printDNSRecords(dnsRecords)
+        getIPFromDNSRecord(dnsRecordsDict, record)
+
+
+def getIPFromDNSRecord(dnsRecords, record):
+    global DOMAIN_IP
+    if record == "A" and  DOMAIN_IP == None:
+        DOMAIN_IP = dnsRecords[0]["value"]
+        print(DOMAIN_IP)
 
 
 def getDNSRecordsByDomain(domain, recordType):
@@ -29,31 +40,33 @@ def getDNSRecordsByDomain(domain, recordType):
     recordType += "/"
     url = baseUrl + recordType + domain
     dnsRecordsRaw = sendGetRequest(url)
-
     return dnsRecordsRaw
 
 
 def printDNSRecords(dnsRecordsRaw):
-    dnsRecords = json.loads(dnsRecordsRaw)
+    dnsRecords = convertStringToJSON(dnsRecordsRaw)
     for dnsRecord in dnsRecords:
         try:
             print(dnsRecord["type"] + "\t" + dnsRecord["value"] + "\t" + dnsRecord["name"])
         except:
             print("Some DNS records may be missing..")
+    return dnsRecords
+
+
+def convertStringToJSON(string):
+    return json.loads(string)
 
 
 def getWhoisRawByDomain(domain):
     url = BASE_WHOIS_URL + domain
     whoisResponseRaw = sendGetRequest(url)
-
     return whoisResponseRaw
 
 
 def printRegistrantInfo(registrantInfo, domain):
     print("Created:\t" + registrantInfo["created"] + " | " + registrantInfo["organisation"] + "\n"
         "Location:\t" + registrantInfo["country"] + ", " + registrantInfo["state"] + ", " + registrantInfo["city"] + "\n"
-        "More:\t\t" + BASE_WHOIS_URL + domain
-          )
+        "More:\t\t" + BASE_WHOIS_URL + domain)
 
 
 def getRegistrantInfo(whoisRawData):
@@ -80,7 +93,6 @@ def replaceEmptyItemsWithPlaceholders(registrantInfoValues):
     for index, info in enumerate(registrantInfoValues):
         if info is None:
             registrantInfoValues[index] = "-"
-
     return registrantInfoValues
 
 
@@ -90,14 +102,46 @@ def getRegistrantDataByAttribute(whoisRawData, attribute):
         return str(value).strip()
 
 
+def scanIP(ip):
+    "https://mxtoolbox.com/SuperTool.aspx?action=scan%3a134.213.168.37&run=networktools"
+    response = sendGetRequest("https://mxtoolbox.com/SuperTool.aspx?action=scan%3a134.213.168.37&run=networktools")
+    return response
+
+
+def getThreatReportsByIP(ip):
+    url = BASE_CYMON_URL + ip + "/events"
+    response = sendGetRequest(url)
+    threatsReportsRaw = convertStringToJSON(response)
+    return threatsReportsRaw
+
+
+def printThreatReports(threatReports):
+    if threatReports["count"] > 0:
+        print("\n\n[*] Printing threat reports for the domain, based on the IP: " + DOMAIN_IP)
+
+        for report in threatReports["results"]:
+            print("[!] " + report["created"] + " " + report["title"] + " " + str(report["description"]).strip().replace(".","[.]"))
+            containsDetailsUrl = (report["details_url"] != None)
+
+            if containsDetailsUrl:
+                    print("More: " + report["details_url"] + "\n")
+    else:
+        print("\n\n[*] No threat reports found for the domain, based on the IP: " + DOMAIN_IP)
+
+
 def main():
+    global DOMAIN_IP
+    DOMAIN_IP = None
     if sys.argv.__len__() > 1:
         domain = sys.argv[1]
         fetchAllDNSRecordsByDomain(domain)
         whoisDomainRaw = getWhoisRawByDomain(domain)
         registrantInfo = getRegistrantInfo(whoisDomainRaw)
         printRegistrantInfo(registrantInfo, domain)
+        threatReports = getThreatReportsByIP(DOMAIN_IP)
+        printThreatReports(threatReports)
     else:
         print("Supply a domain like so: whois.py [domain]")
+
 
 main()
