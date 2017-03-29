b93dda33fc8f899ff98dcad21f37f0b38f6bb4c1
Mantvydas Baranauskas <mantvydo@gmail.com>
2017-03-29 20:47:35 +0100

    Update index.php

--- sourcer/index.php
+++ sourcer/index.php
@@ -9,7 +9,7 @@ function getContentByUrl($url) {
 	curl_setopt($curlHandle, CURLOPT_URL, $url);
 	curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curlHandle,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+	curl_setopt($curlHandle, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 	curl_setopt($curlHandle, CURLOPT_HEADERFUNCTION, "handleHeaderCallback");
 	$response = curl_exec($curlHandle);
 	curl_close($curlHandle);
