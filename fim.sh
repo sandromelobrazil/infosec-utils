40092259457f9179050a6846cdf310e35b222bca
mantvydas <mantvydas@localhost.localdomain>
2017-04-03 18:29:05 +0100

    added fim bash version

---
+++ fim.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# directories should be separated by spaces and have the ending slash /
+MONITORED_FOLDERS=(~/Downloads)
+MONITORED_FILES_SAVE_LOCATION=monitored-files
+FIM_BASELINE_FILE=fim-baseline
+
+if [ -f $FIM_BASELINE_FILE ]
+then
+    fimReportFile=fim-report-`date +%Y-%m-%d-%H-%M-%S`
+else
+    fimReportFile=$FIM_BASELINE_FILE
+fi
+
+for folder in ${MONITORED_FOLDERS[@])}
+do  
+    echo "[*] Calculating hashes for files in" $folder
+    find $folder* -type f > $MONITORED_FILES_SAVE_LOCATION
+
+    while read -r line; do
+        echo $(md5sum "$line") >> $fimReportFile
+    done < $MONITORED_FILES_SAVE_LOCATION
+done
+
+rm $MONITORED_FILES_SAVE_LOCATION
+echo "[*] FIM report saved in" $fimReportFile
+echo "[*] Comparing $fimReportFile with $FIM_BASELINE_FILE"
+diff $FIM_BASELINE_FILE $fimReportFile
+
+
+
