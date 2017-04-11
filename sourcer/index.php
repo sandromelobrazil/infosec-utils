6f322d39a86fa751ed340551875f1a10c5549427
mantvydas <mantvydo@gmail.com>
2017-04-11 20:23:53 +0100

    added cookies to curl request

--- sourcer/index.php
+++ sourcer/index.php
@@ -10,6 +10,7 @@ function getContentByUrl($url) {
 	curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($curlHandle, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+	curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array("Cookie: youaskedfor=cookie"));	
 	curl_setopt($curlHandle, CURLOPT_HEADERFUNCTION, "handleHeaderCallback");
 	$response = curl_exec($curlHandle);
 	curl_close($curlHandle);
