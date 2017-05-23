2176d73348a714e0d1ec11079563d48ceb172816
Mantvydas Baranauskas <mantvydo@gmail.com>
2017-05-23 17:25:39 +0100

    added some date from talos intelligence
    - improved formatting

--- whois.py
+++ whois.py
@@ -8,8 +8,8 @@ from urllib.error import HTTPError
 
 BASE_WHOIS_URL = "https://www.whois.com/whois/"
 BASE_CYMON_URL = "https://cymon.io/api/nexus/v1/ip/"
-
-# todo senderbase IP reputation
+BASE_TALOS_URL = "https://talosintelligence.com/sb_api/"
+BASE_ABUSEIP_URL = "https://www.abuseipdb.com/check/"
 
 def sendHTTPRequest(request):
     try:
@@ -24,7 +24,7 @@ def sendHTTPRequest(request):
     return response
 
 
-def buildHTTPRequest(url, headers={"Authorization": "Token 9dd9bc3276b0c35f5d64624bb7901f296b0ff37a", "Accept": "application/json", "Content-Type": "text/html; charset=utf-8"}, data=None):
+def buildHTTPRequest(url, headers={"Authorization": "Token 9dd9bc3276b0c35f5d64624bb7901f296b0ff37a", "User-Agent": "Mantvydas' cmd-line Whois", "Accept": "application/json", "Content-Type": "text/html; charset=utf-8"}, data=None):
     httpRequest = urllib.request.Request(url, data=data, headers=headers)
     return httpRequest
 
@@ -37,6 +37,7 @@ def fetchAllDNSRecordsByDomain(domain):
         "TXT",
         "SOA"
     ]
+    printSection("DNS Lookup")
 
     for record in recordsToLookUp:
         dnsRecords = getDNSRecordsByDomain(domain, record)
@@ -64,7 +65,7 @@ def printDomains(domains):
     domains = convertStringToJSON(domains)
     
     if domains["count"] > 0:
-        print("\n[*] Some associated domains:")
+        printSection("Associated Domains")
         for domain in domains["results"]:
             print(domain["name"] + "\t\t(created: " + domain["created"] + ")")
     else:
@@ -82,7 +83,7 @@ def getDNSRecordsByDomain(domain, recordType):
 
 def printDNSRecords(dnsRecordsRaw, record):
     dnsRecords = convertStringToJSON(dnsRecordsRaw)
-
+    
     for dnsRecord in dnsRecords:
         try:
             print(dnsRecord["type"] + "\t" + dnsRecord["value"] + "\t" + dnsRecord["name"])
@@ -95,32 +96,30 @@ def convertStringToJSON(string):
     return json.loads(string)
 
 
-def getWhoisRawByDomain(domain):
+def getRawWhois(domain):
     url = BASE_WHOIS_URL + domain
     request = buildHTTPRequest(url)
     whoisResponseRaw = sendHTTPRequest(request)
     return whoisResponseRaw
 
 
-def printRegistrantInfo(registrantInfo, domain):
+def printDoimainWhois(registrantInfo, domain):
     registrantInfo = registrantInfo.split('<pre class="df-raw"')
     
     if len(registrantInfo) > 1:
-        registrantInfo = registrantInfo[1].split('</pre>')[0]
+        registrantInfo = registrantInfo[1].split('</pre>')[0].replace("\n\n\n","\n")
         lines = registrantInfo.split("\n")
         registrantInfo = ""
         
-        for line in lines[1:]:
+        for line in lines[1:len(lines)-1]:
             registrantInfo += line + "\n"
+        printSection("Domain Whois Info")
+        print(registrantInfo)
 
-        print("\n[*] Whois information: \n" + registrantInfo)
 
-
-def replaceEmptyItemsWithPlaceholders(registrantInfoValues):
-    for index, info in enumerate(registrantInfoValues):
-        if info is None:
-            registrantInfoValues[index] = "-"
-    return registrantInfoValues
+def printSection(sectionName):
+    separator = "########################################################################"
+    print("\n\n\n" + separator + "\n    " + sectionName + "\n" + separator)
 
 
 def getThreatReportsByIP(ip):
@@ -136,18 +135,13 @@ def getThreatReportsByIP(ip):
 
 
 def printThreatReports(threatReports):
+    printSection("Associated Threat Reports")
+    
     if threatReports["count"] > 0:
-        print("\n[*] Some historical threat reports for " + DOMAIN_IP + ":")
-
         for report in threatReports["results"]:
-            print("[!] " + report["created"] + " " + report["title"] + " " + str(report["description"]).replace(".","[.]").strip())
-            containsDetailsUrl = (report["details_url"] != None)
-
-            if containsDetailsUrl:
-                print("More: " + report["details_url"])
-            print("\r")
+            print("[!] " + report["created"] + " " + report["title"])
     else:
-        print("\n[*] No associated threat reports found")
+        print("[*] No associated threat reports found")
 
 
 def isValidIPAddress(string):
@@ -173,21 +167,51 @@ def getHostNameByIp(IP):
 
 def extractCanonicalName(string):
     anchor = "canonical name: "
-    
     if anchor in string:
         hostname = string.split(anchor)[1]
         hostname = hostname.split("<br/>")[0]
     else:
         hostname = None
-
     return hostname
 
 
 def printHostName(hostname):
+    printSection("IP to Hostname")
     if hostname != None:
-        print("[*] Resolved IP to hostname " + hostname)
+        print(hostname)
     else:
         print("\n[*] Could not resolve IP to hostname...")
+
+
+def getWBRS(IP):
+    url = BASE_TALOS_URL + "remote_lookup?hostname=SDS&query_string=%2Fscore%2Fwbrs%2Fjson%3Furl=" + IP
+    request = buildHTTPRequest(url)
+    response = convertStringToJSON(sendHTTPRequest(request))
+    wbrs = str(response[0]["response"]["wbrs"]["score"])
+    return wbrs
+
+
+def getTalosDetails(IP):
+    url = BASE_TALOS_URL + "query_lookup?query=%2Fapi%2Fv2%2Fdetails%2Fip%2F&query_entry=" + IP
+    request = buildHTTPRequest(url)
+    response = convertStringToJSON(sendHTTPRequest(request))
+    return response
+
+
+def getTalosIntelligenceReport(IP):
+    printSection("Talos Intelligence + AbuseIPDB")
+    talosDetails = getTalosDetails(IP)
+    report = "[*] Web score: " + talosDetails["web_score_name"] + "\n" + "[*] Email score: " + talosDetails["email_score_name"] + "\n"
+    report += "[*] WBRS: " + getWBRS(IP)
+    print(report)
+
+
+def getAbuseIpReport(IP):
+    days = "90"
+    endpoint = BASE_ABUSEIP_URL + IP + "/json?key=J6uXCXZUfK9axKh3I1sGi1S467vSqgPR5CWNcUFp&days=" + days
+    request = buildHTTPRequest(endpoint)
+    response = convertStringToJSON(sendHTTPRequest(request))
+    print("[*] Times IP reported as abusive in the last " + days + " days: " + str(len(response)) + ", http://www.abuseipdb.com/check/" + IP)
 
 
 def main():
@@ -195,21 +219,23 @@ def main():
     DOMAIN_IP = None
 
     if sys.argv.__len__() > 1:
-        domain = sys.argv[1]
+        host = sys.argv[1]
 
-        if not isValidIPAddress(domain):
-            fetchAllDNSRecordsByDomain(domain)
-        else:
-            DOMAIN_IP = domain
+        if isValidIPAddress(host):
+            DOMAIN_IP = host
             hostname = getHostNameByIp(DOMAIN_IP)
             printHostName(hostname)
             domainsRaw = getDomainsByIP(DOMAIN_IP)
             printDomains(domainsRaw)
+        else:
+            fetchAllDNSRecordsByDomain(host)
 
-        whoisDomainRaw = getWhoisRawByDomain(domain)
-        printRegistrantInfo(whoisDomainRaw, domain)
+        whoisDomainRaw = getRawWhois(host)
+        printDoimainWhois(whoisDomainRaw, host)
         threatReports = getThreatReportsByIP(DOMAIN_IP)
         printThreatReports(threatReports)
+        getTalosIntelligenceReport(DOMAIN_IP)
+        getAbuseIpReport(DOMAIN_IP)
     else:
         print("Supply a domain or IPv4 like so: whois.py [domain | IP]")
 
