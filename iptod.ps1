142a7d9af84a37117286269e3fd6a42837865dcb
Mantvydas Baranauskas <mantvydo@gmail.com>
2017-05-26 11:51:54 +0100

    added ip to domain resolver

---
+++ iptod.ps1
@@ -0,0 +1,35 @@
+param (
+    [string] $ip
+)
+
+function processArguments() {
+    if ($ip -eq "") {
+        Write-Host "[i] iptod (IP to Domain) usage: iptod.ps1 <IP>. Example: iptod.ps1 127.0.0.1"
+        break
+    }
+}
+
+function main() {
+    processArguments
+    Set-Location $MyInvocation.PSScriptRoot
+    [array] $dnsRecords = ConvertFrom-Csv (Get-Content zones.csv)
+    getDomainByIp $ip $dnsRecords
+}
+
+function getDomainByIp($ip, $dnsRecords) {
+    $isResolved = $false
+    Write-Host "[*] Searching..."
+
+    foreach ($record in $dnsRecords) {
+        if ($record.ip -eq $ip) {
+            Write-Host $record.domain
+            $isResolved = $true
+        }
+    }
+
+    if (!$isResolved) {
+        Write-Host "[!] No DNS records found for $ip"
+    }
+}
+
+main
