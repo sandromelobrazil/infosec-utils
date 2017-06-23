8d2670505796057f173630cd0437144e0b9e074e
Mantvydas Baranauskas <mantvydo@gmail.com>
2017-06-23 14:20:21 +0100

    fixed checks for userId, housekeeping

--- renum/renum-v0.4.ps1
+++ renum/renum-v0.4.ps1
@@ -30,13 +30,13 @@ param(
     [switch]$programs,
     [switch]$mft,
     [switch]$mailfile,
+    [switch]$mailfileo,
     [switch]$winver,
     [switch]$sniffer,
     [string]$module,
     [string]$modargs,
     [switch]$h
 )
-# make snifffer save to PCAP
 
 # If these not set, you will be prompted for your service id credentials. Setting these is not encouraged.
 $Global:SERVICE_ID = ""
@@ -45,12 +45,14 @@ $Global:SESSION = ""
 $Global:COMMAND_SPECIFIED = $false
 $Global:MODS_PATH = ".\mods\"
 $Global:UTILS_PATH = ".\utils\"
+$Global:ARTEFACTS_PATH = "C:\artefacts\"
 
-function ConstructModuleObject([string] $_name, [string] $_helpText, $_shouldExecute, [string] $_arguments = $false) {
+function ConstructModuleObject([string] $_name, [string] $_helpText, $_shouldExecute, [string] $_arguments = $false, [bool] $_userIDRequired = $false) {
     $moduleObject = @{
         name = $_name
         helpText = $_helpText
         shouldExecute = $_shouldExecute
+        isUserIDRequired = $_userIDRequired
         arguments = $_arguments
     }
     return $moduleObject
@@ -64,7 +66,7 @@ $Global:MODULES = [array] (
     (ConstructModuleObject "connections" "-conns`t`t Get established connections & ports listening" $conns),
     (ConstructModuleObject "users" "-users`t`t Get users who have used the machine" $users),
     (ConstructModuleObject "regquery" "-regquery`t Get registry key info. Use -key to specify the 'key'. Mind the quotes." $regquery $key),
-    (ConstructModuleObject "autoruns" "-autoruns`t Get autoruns from popular persistence locations. Requires -user <username>" $autoruns $user),
+    (ConstructModuleObject "autoruns" "-autoruns`t Get autoruns from popular persistence locations. Requires -user <username>" $autoruns $user $true),
     (ConstructModuleObject "mounteddevices" "-mountedd`t Get currently mounted physical device letters" $mountedd),
     (ConstructModuleObject "mountedshares" "-mounteds`t Get currently mounted shares" $mounteds),
     (ConstructModuleObject "programs" "-programs`t Get currently installed programs" $programs),
@@ -72,19 +74,20 @@ $Global:MODULES = [array] (
     (ConstructModuleObject "module" "-module`t Specify path of an external module to be executed. -modargs to supply arguments" $module $modargs),
     (ConstructModuleObject "drivers" "-drivers`t Get installed drivers" $drivers),
     (ConstructModuleObject "mailfile" "-mailfile`t Open domino mailfile. Requires -user <username>" $mailfile),
+    # (ConstructModuleObject "mailfileo" "-mailfileo`t Open O365 mailfile. Requires -user <username>" $mailfileo),
     (ConstructModuleObject "usbenumeration" "-usbenum`t Get USB devices that had been plugged in" $usbenum),
     (ConstructModuleObject "sniffer" "-sniffer`t Sniff traffic. Currently dumps results to to CSV only." $sniffer),
     (ConstructModuleObject "netbioscache" "-nbtcache`t Get NetBios cache" $nbtcache),
     (ConstructModuleObject "accounts" "-accounts`t Get user accounts for machine" $accounts),
     (ConstructModuleObject "smbsessions" "-sessions`t Get a list of SMB sessions incoming to the remote machine" $sessions),
     (ConstructModuleObject "netstats" "-netstats`t Get uptime, permissions and password violations count" $netstats),
-    (ConstructModuleObject "typedurls" "-typedurls`t Get URLs  typed in Internet Explorer address bar. Requires -user <username>" $typedurls $user),
-    (ConstructModuleObject "downloads" "-downloads`t Get contents of the downloads folder. Requires -user <username>" $downloads $user),
-    (ConstructModuleObject "desktop" "-desktop`t Get contents of the desktop. Requires -user <username>" $desktop $user),
+    (ConstructModuleObject "typedurls" "-typedurls`t Get URLs  typed in Internet Explorer address bar. Requires -user <username>" $typedurls $user $true),
+    (ConstructModuleObject "downloads" "-downloads`t Get contents of the downloads folder. Requires -user <username>" $downloads $user $true),
+    (ConstructModuleObject "desktop" "-desktop`t Get contents of the desktop. Requires -user <username>" $desktop $user $true),
     (ConstructModuleObject "mft" "-mft`t`t Get Master File Table" $mft),
     (ConstructModuleObject "dnscache" "-dnscache`t Get DNS cache" $dnscache),
     (ConstructModuleObject "windowsversion" "-winver`t Get Windows OS version, including Service Pack" $winver),
-    (ConstructModuleObject "recent" "-recent`t Get recently accessed documents. Requires -user <username>" $recent $user)
+    (ConstructModuleObject "recent" "-recent`t Get recently accessed documents. Requires -user <username>" $recent $user $true)
 )
 
 function main() {
@@ -169,11 +172,15 @@ function isUserSpecified() {
     }
 }
 
-function sniffTraffic() {
-    $command = "sniffer"
+function sniffTraffic($module) {
     $ignoreIP = (Test-Connection -ComputerName (hostname) -Count 1 | Select IPV4Address).IPV4Address.IPAddressToString
-    executeRemoteCommand $command $ignoreIP
-    waitForEscapeKey
+    executeRemoteCommand $module $ignoreIP
+    $packetCaptureFileLocation = waitForEscapeKey
+    identifySuspiciousTraffic $packetCaptureFileLocation
+}
+
+function identifySuspiciousTraffic($packetCaptureFileLocation) {
+    powershell $Global:UTILS_PATH\find-suspicious-traffic.ps1 $Global:ARTEFACTS_PATH\$packetCaptureFileLocation
 }
 
 function waitForEscapeKey() {
@@ -181,7 +188,7 @@ function waitForEscapeKey() {
     [date] $sniffingStartTime = (Get-Date)
 
     while ($true) {
-        Write-Progress -Activity "Sniffing $Global:REMOTE_HOST... Press ESC or Ctrl+C to stop sniffing and download the results..." -Status ((Get-Date) - $sniffingStartTime) -PercentComplete $percent
+        Write-Progress -Activity "Sniffing traffic $Global:REMOTE_HOST... Press ESC or Ctrl+C to stop and download the results..." -Status ((Get-Date) - $sniffingStartTime) -PercentComplete $percent
 
         if ($host.ui.RawUi.KeyAvailable) {
             $pressedKey = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
@@ -189,9 +196,11 @@ function waitForEscapeKey() {
             if ($pressedKey.VirtualKeyCode -eq 27 -or (3 -eq [int]$Host.UI.RawUI.ReadKey("AllowCtrlC,IncludeKeyUp,NoEcho").Character)) {
                 Get-Job | Stop-Job 
                 Get-Job | Remove-Job
-                $artefact = downloadArtefact "$Global:REMOTE_HOST-sniff.csv"
+                $packetCaptureFilename = "$Global:REMOTE_HOST-sniff.csv"
+                $artefact = downloadArtefact $packetCaptureFilename
                 Write-Host "[*] Download complete."
                 Remove-Item $artefact
+                return $packetCaptureFilename
                 break
             }
         } 
@@ -214,11 +223,11 @@ function getMFT() {
 }
 
 function parseMFT() {
-    $mftLocation = "C:\artefacts\$Global:REMOTE_HOST.mft"
-    $mftReport = "C:\artefacts\$Global:REMOTE_HOST-mft.xls"
+    $mftLocation = "$Global:ARTEFACTS_PATH\$Global:REMOTE_HOST.mft"
+    $mftReport = "$Global:ARTEFACTS_PATH\$Global:REMOTE_HOST-mft.xls"
 
     Write-Host "[*] Parsing $Global:REMOTE_HOST.mft..."
-    .\utils\mftdump.exe $mftLocation /o $mftReport
+    powershell $Global:UTILS_PATH\mftdump.exe $mftLocation /o $mftReport
     Write-Host "[*] Opening $Global:REMOTE_HOST MFT, please wait..."
     Invoke-Item $mftReport
 }
@@ -246,6 +255,19 @@ function getMailFile() {
     Invoke-Item $mailfileDestination
 }
 
+function getOutlookMailFile() {
+    $user = isUserSpecified
+    $Global:COMMAND_SPECIFIED = $true
+    $mailfile = "*.ost"
+    $mailfileDestination = "C:\Users\$env:USERNAME\Downloads\"
+    $mailfileSource = "\\" + "$Global:REMOTE_HOST\c$\users\$user\AppData\Local\Microsoft\Outlook\*.ost"
+    
+    Write-Host "[*] Downloading mailfile to $mailfileDestination. Please wait, it will be opened automatically..."
+    Copy-Item $mailfileSource -Destination $mailfileDestination -Force -Recurse
+    Write-Host [*] Opening mailfile... Do not forget to delete it once done.
+    Invoke-Item $mailfileDestination
+}
+
 function getDrivers() {
     $Global:COMMAND_SPECIFIED = $true
     driverquery /s $Global:REMOTE_HOST /u $Global:SERVICE_ID /p $Global:PASSWORD
@@ -259,10 +281,11 @@ function enumerateSystem() {
                 "mft" { getMFT }
                 "drivers" { getDrivers }
                 "mailfile" { getMailFile }
-                "sniffer" { sniffTraffic }
+                "mailfileo" { getOutlookMailFile }
+                "sniffer" { sniffTraffic $module }
                 "module" { executeExternalModule $module $modargs }
-                Default { executeRemoteCommand $module.name $module.arguments }
-            }
+                Default { executeRemoteCommand $module $module }
+            }            
         }
     }
     
@@ -290,10 +313,15 @@ function changeWorkingDirectory() {
 
 function executeRemoteCommand($module, $arguments=$null, $isExternalModule=$false) {
     $Global:COMMAND_SPECIFIED = $true
-    $command = $module
+    $command = $module.name
+
+    if ($module.isUserIDRequired -and $user -eq "") {
+        $module.arguments = isUserSpecified
+    }
+    $arguments = $module.arguments
 
     if (!$isExternalModule) {
-        $module = "mods\$command.ps1"
+        $module = "$Global:MODS_PATH\$command.ps1"
     }
     
     if ($command -eq "sniffer") {
@@ -304,7 +332,7 @@ function executeRemoteCommand($module, $arguments=$null, $isExternalModule=$fals
 }
 
 function downloadArtefact($artefactName, $isLocked=$false) {
-    $artefactsSaveLocation = "C:\artefacts\" + $artefactName
+    $artefactsSaveLocation = "$Global:ARTEFACTS_PATH$artefactName"
     $artefactSourceLocation = "\\" + $Global:REMOTE_HOST + "\c$\temp\$artefactName"
     Write-Host "[*] Downloading $artefactName to $artefactsSaveLocation"
     Copy-Item $artefactSourceLocation -Destination $artefactsSaveLocation -Force -Recurse
@@ -312,7 +340,7 @@ function downloadArtefact($artefactName, $isLocked=$false) {
 }
 
 function copyUtilsToRemoteHost() {
-    Copy-Item .\utils\copy.exe -Destination \\$Global:REMOTE_HOST\c$\TEMP\copy.exe -Recurse -Force
+    Copy-Item $Global:UTILS_PATH\copy.exe -Destination \\$Global:REMOTE_HOST\c$\TEMP\copy.exe -Recurse -Force
 }
 
 function notifyConnectionError() {
@@ -324,7 +352,7 @@ function establishRemoteSession() {
     Write-Host "[*] Connecting to" $Global:REMOTE_HOST
     cmdkey.exe /add:$Global:REMOTE_HOST /user:$Global:SERVICE_ID /pass:$Global:PASSWORD | Out-Null
 
-    if (!$mailfile) {
+    if (!$mailfile -and $Global:REMOTE_HOST.Length -gt 7) {
         $Global:SESSION = New-PSSession -ComputerName $Global:REMOTE_HOST -Credential $Global:CREDENTIALS
         if ($Global:SESSION) {
             Write-Host "[*] Connected!"
