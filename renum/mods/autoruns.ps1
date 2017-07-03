e9c7ed798a0beb4ba879f21f5ff309ba28c82a5f
Mantvydas Baranauskas <mantvydo@gmail.com>
2017-07-03 17:18:54 +0100

    added more persisence locations and improved formatting

--- renum/mods/autoruns.ps1
+++ renum/mods/autoruns.ps1
@@ -1,28 +1,49 @@
 param(
     [string] $user
 )
+$SEPARATOR = "`n_____________________________________________________________________________________________`n"
+
+function printSeparator($messageString) {
+    Write-Host $SEPARATOR [*] $messageString...`n -ForegroundColor Yellow
+}
 
 $SID = ((New-Object System.Security.Principal.NTAccount($user)).Translate([System.Security.Principal.SecurityIdentifier])).value
+Write-Host "[*] Retrieving registry keys for user $user (SID $SID)`n"
 
 [array] $PATHS = (
     "HKEY_USERS\$SID\Software\Microsoft\Windows\CurrentVersion\Run\",
     "HKEY_USERS\$SID\Software\Microsoft\Windows\CurrentVersion\RunOnce\",
     "HKLM\Software\Microsoft\Windows\CurrentVersion\Run\",
-    "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce\"
-    # "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\Notify"
-    # "REGISTRY::HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\\Shell",
-    # "REGISTRY::HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows\\AppInit_DLLs"
+    "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce\",
+    "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon",
+    "HKEY_USERS\$SID\Software\Microsoft\Windows NT\CurrentVersion\Winlogon",
+    "HKEY_USERS\$SID\Software\Microsoft\Windows NT\CurrentVersion\Windows",
+    "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows"
     )
 
 foreach ($path in $PATHS) {
+    printSeparator "Retrieving $path"
     reg query $path
-    Write-Host
 }
 
-schtasks.exe
-# todo filter our known scheduled tasks / autoruns to show the unusual ones.
 
-# HKLM\System\CurrentControlSet\Services
+function getServices() {
+    printSeparator "Retrieving services"
+    $services = Get-ChildItem -Path HKLM:\System\CurrentControlSet\Services | ForEach-Object { 
+        Get-ItemProperty $_.PsPath
+    } 
+    
+    $services | Sort-Object -Property "ImagePath" | ForEach-Object {
+        if ($_.ImagePath -notlike "*.sys" -and $_.ImagePath -ne "") {
+            $_.ImagePath -replace ('"', "")
+        }
+    }
+}
+
+getServices
+printSeparator "Retrieving scheduled tasks"
+schtasks.exe
+
 # HKLM\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce
 # HKCU\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce
 # HKLM\Software\Microsoft\Windows\CurrentVersion\RunServices
@@ -35,4 +56,4 @@ schtasks.exe
 # HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnceEx
 # HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows\load
 # HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows
-# HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows\\AppInit_DLLs
+# HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows\AppInit_DLLs
