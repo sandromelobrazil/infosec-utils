8aab2337c14c65ec16c6f9a75c91bc9198a91569
Mantvydas Baranauskas <mantvydo@gmail.com>
2017-07-04 12:13:21 +0100

    added ability to see image paths for running processes

--- renum/mods/tasklist.ps1
+++ renum/mods/tasklist.ps1
@@ -1,1 +1,9 @@
-tasklist /SVC
+Write-Host [*] Retrieving running processes... -ForegroundColor Yellow
+tasklist /SVC | sort
+
+function getProcessImagePath() {
+    Write-Host `n`n[*] Retrieving image paths for running processes... -ForegroundColor Yellow
+    Get-Process  | Sort-Object -Property "Path" | Format-Table Path, ProcessName -AutoSize 
+}
+
+getProcessImagePath
